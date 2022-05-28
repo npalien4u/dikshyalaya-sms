610e9007564d9c16a58382479be3dbc42d54216b
npalien4u <npalien4u@gmail.com>
2022-05-28 13:55:38 +0530

    - initial commit
    initial unchanged commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\SmLanguagePhrase;
+use App\SmModule;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
+use Modules\MenuManage\Entities\Sidebar;
+use Modules\RolePermission\Entities\InfixModuleInfo;
+use Modules\RolePermission\Entities\InfixModuleStudentParentInfo;
+use SpondonIt\Rpclient\RaudhahPay;
+
+if (config('app.app_sync')) {
+    Route::get('/', 'LandingController@index')->name('/');
+}
+
+if (moduleStatusCheck('Saas')) {
+    Route::group(['middleware' => ['subdomain'], 'domain' => '{subdomain}.' . config('app.short_url')], function ($routes) {
+        require ('tenant.php');
+    });
+
+    Route::group(['middleware' => ['subdomain'], 'domain' => '{subdomain}'], function ($routes) {
+        require ('tenant.php');
+    });
+}
+
+Route::group(['middleware' => ['subdomain']], function ($routes) {
+    require ('tenant.php');
+});
+
